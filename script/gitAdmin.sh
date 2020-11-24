dab49dc5e7364dc14d7256ff46ea7194cc4f4020
Luis Puente Fernandez <luis.puefer@educa.jcyl.es>
2020-11-24 08:57:19 +0000

    Comentados los archivos de configuracion

--- script/gitAdmin.sh
+++ script/gitAdmin.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-directorio=/git
+#Script para crear repositorios accesibles de forma remota, mediante el usuario git
+directorio=/git #Directorio donde se encuentran los repositorios 
 
-function ayuda(){
+function ayuda(){ #En caso de error, o que se introduzca un parametro mal, mostrara el mensaje de ayuda
 	echo "Script para la creación y borrado de repositorios git:
 	-n Nombre, crea un repositorio, en caso existir en el repositorio muestra un mensaje de error
 	-b Nombre, borra un repositorio, caso de no existir muestra un mensaje de erro
@@ -10,29 +11,29 @@ function ayuda(){
 	";
 }
 
-function crear(){
-	ruta=${directorio}/${1}.git
-	if [[ ! -d "$ruta" ]]; then
-		mkdir $ruta
-		git init --bare $ruta > /dev/null 2>&1
+function crear(){ #Se encarga de crear el repositorio
+	ruta=${directorio}/${1}.git #Crea la variable ruta, que es la ruta absoluta al directorio
+	if [[ ! -d "$ruta" ]]; then #Comprueba que la ruta no exista en caso de existir lo muestra por pantalla
+		mkdir $ruta #Crea el directorio
+		git init --bare $ruta > /dev/null 2>&1 #Inicia el repositorio, vacio, en la ruta expecificada, redirigelos stderr y stdout a /dev/null
 
-		[[ $? -eq 0 ]] && echo "Repositorio creado" || echo "Error, algo ha fallado al crear el repositorio. Codigo de error: $?"
+		[[ $? -eq 0 ]] && echo "Repositorio creado" || echo "Error, algo ha fallado al crear el repositorio. Codigo de error: $?" #En funcion del resultado del ultimo comando $? muestra el mensaje de error o no
 
 	else
 		echo "El repositorio $1 ya existe"
 	fi
-	unset ruta
+	unset ruta #Elimina la variable ruta 
 }
 
-function borrar(){
-	ruta=${directorio}/${1}.git
-	if [[ -d "$ruta" ]]; then
-		read -p "Quieres borrar el repositorio ( s | S ): " borra
-		borra=$(tr '[:upper:]' '[:lower:]' <<< $borra);
-		if [[ $borra == "s" ]]; then
-			rm -r $ruta > /dev/null 2>&1 
+function borrar(){ #Se encarga de borra el repositorio
+	ruta=${directorio}/${1}.git #Crea la variable ruta, para almacenar al ruta absoluta
+	if [[ -d "$ruta" ]]; then #Comprueba que exista el repositorio 
+		read -p "Quieres borrar el repositorio ( s | S ): " borra #Pregunta a el usuario si quiere borra el repositorio, almacena la respuesta en borra
+		borra=$(tr '[:upper:]' '[:lower:]' <<< $borra); #Pasa las mayusculas a minusculas con el comando tr
+		if [[ $borra == "s" ]]; then #Comprueba que la variable borra sea igual a s
+			rm -r $ruta > /dev/null 2>&1 #Borra el repositorio y su contendio con redirecinamiento de la salida a /dev/null
 
-			[[ $? -eq 0 ]] && echo "Repositorio borrado" || echo "Error, algo ha fallado al borrar el repositorio. Codigo de error: $?"
+			[[ $? -eq 0 ]] && echo "Repositorio borrado" || echo "Error, algo ha fallado al borrar el repositorio. Codigo de error: $?" #En función del estado del ultimo comando muestra un mensaje
 			
 		else
 			echo "No se ha borrado el repositorio"
@@ -43,12 +44,12 @@ function borrar(){
 
 }
 
-while getopts ':n:b: hv' flag; do
+while getopts ':n:b: hv' flag; do #Con getops se capturan las "flags para llamar a las respectivas funciones
 	case $flag in
-		n) crear $OPTARG;;
+		n) crear $OPTARG;; 
 		b) borrar $OPTARG;;
 		v) echo "versión 0.5";;
-		\? | \: | h) ayuda;;
+		\? | \: | h) ayuda;; 
 
 	esac
 
