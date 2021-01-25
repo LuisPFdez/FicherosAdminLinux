b19581214b0268c84bdebfeeab329743e8d17076
Luis Puente Fernandez <luis.puefer@educa.jcyl.es>
2021-01-25 11:56:32 +0000

    Resuelto varios fallos de phpDocInstalador.sh

--- script/phpDocInstalador.sh
+++ script/phpDocInstalador.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+actualizado=0
 if [[ $(id -u) -eq 0 ]]; then
-	php --version > /dev/null
+	php --version 2> /dev/null
 	if [[ $? -ne 0 ]]; then
 		echo "PHP no esta instalado, instalando PHP 7.4"
-		apt update > /dev/null 2&>1
-		apt install php7.4 -y > /dev/null 2&>1
+		actualizado=1
+		apt update 
+		apt install php7.4 -y 
 	fi
 	if [[ $(php -m | grep "SimpleXML" | wc -l) -eq 0 ]]; then
 		echo "Descargando el modulo xml para php"
-		apt install php-xml -y > /dev/null 2&>1
+		if [[ $actualizado -eq 0 ]] && apt update
+		apt install php-xml -y 
 	fi
 	phpenmod xml
 	if [[ ! -f ./phpDocumentor.phar ]]; then
