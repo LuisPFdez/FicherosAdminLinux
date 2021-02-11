c926ce4060381b1b86f5c4a8f216aefb94a8b743
Luis Puente Fernandez <luis.puefer@educa.jcyl.es>
2021-02-11 09:34:23 +0000

    Primera versión estable

--- Readme.md
+++ Readme.md
@@ -1,7 +1,23 @@
 # Ficheros de administración para Linux
-Conjunto de script, servicios y ficheros de configuracion para linux
+Conjunto de script, servicios y ficheros de configuración para linux
 
 ## Servicios
-*vaciadoTmp = Servicio que tiene asignado un timer, se encarga unicamente de llamar a un script para vaciar los ficheros de las carpetas tmp del servidor
-
-##Scripts
+* vaciadoTmp -> Servicio que tiene asignado un timer, se encarga unicamente de llamar a un script para vaciar los ficheros de las carpetas tmp del servidor
+* vaciadoTmp **(timer)** -> Se ejecuta el servicio cada hora
+## Scripts
+* IP -> Obtiene la dirección ip (4 o 6), y en caso de no estar conectado devuelve un símbolo
+* gitAdmin -> Permite la administración de un servidor de repositorios
+* phpDocInstalador -> Permite la instalacián rapida de phpDocumentor
+* ssl -> Permite crea y autofirmar certificados con openssl
+* vaciadoTmp -> Busca todas las carpetas tmp y borra su contenido
+* preparadoChroot -> Permite la creación rápida de una jaula 
+## Instalador 
+### Permite creación y configuración rápida de un servidor
+Crea el usuario **operadorweb** e instala: 
+	* Apache2 
+	* php7.4
+	* xdebug
+	* mysql
+	* phpmyadmin
+	* bind9
+**Para mysql un usuario con todos los permisos y opción de grant**
--- script/vaciarTmp.sh
+++ script/vaciarTmp.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-
-function listado(){
-  args=$( [[ -z $args ]] && echo $1 || echo $args);
-
-  while read args; do 
-
-    total=$(ls -A $1 | wc -l );
-    if [[ -d $1 && $total -gt 0 ]]; then
-      if [[ $args == $2 ]]; then
-        echo "$1/$args" 
-      fi
-      listado "${1}/${args}" $2
-    fi
-
-  done <<< $(ls -A $1 2> /dev/null);
+#Directorio que tiene que buscar
+directorio=tmp
+#Funcion encargada de buscar la carpeta que se le pase por parametro
+function buscarCarpeta(){
+	if [[ -d $1 ]]; then #Comprueba que el directorio exista
+		rutas=$(find $1 -name $directorio) #Busca todas las carpetas dentro del directorio
+		echo $rutas #Devuelve las rutas obtenidas
+	fi
 }
 
-function main(){
-	for args in $@; do
-		listado $args tmp
-	done
-	
-	#while read args; do
-	#	echo $args
-	#done <<< $(cat /tmp/archivos.txt)
-}
+for i in $@; do #Recorre todos los directorios que se le pasan por parametro
+	rutas=$(buscarCarpeta $i) #LLama a la funcion buscarCarpeta y almacena el resultado en rutas
+	for ruta in $rutas; do #Si buscarCarpeta devuelve algo recorrera lo que ha retornado 
+		rm -r $ruta/* 2>/dev/null 1&>2 #Vacia la ruta, en caso de estar vacio muestra un error por lo que lo redirige a dev/null
+	done	
+
+done
+exit 0 #Cierra el programa con un exit 0 (todo ejecutado de forma correcta)
